ff1e9b17fc640a0736bc38bfaede51e25266c010
William Tsu <30533780+wtsu@users.noreply.github.com>
2024-08-09 10:42:49 -0400

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -2,9 +2,15 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
+        #Url to the github repo
         source="https://github.com/wtsu/prefect-practice.git",
+
+        #name of the python file and the function to run
         entrypoint="rick_and_morty.py:run_rick_and_morty",
     ).deploy(
+        #name of the deployment
         name="rick_and_morty",
+
+        #name of the workpool to use
         work_pool_name="managed1",
     )
